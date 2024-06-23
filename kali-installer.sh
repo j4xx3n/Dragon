b0bd6ba4e85e3fe7c472d92a05f0740c8c81264b
j4xx3n <105862349+j4xx3n@users.noreply.github.com>
2024-06-23 06:22:54 -0500

    Create kali-installer.sh

---
+++ kali-installer.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+sudo apt update
+sudo apt install golang-go -y
+go install github.com/tomnomnom/gf@latest
+git clone https://github.com/NitinYadav00/gf-patterns.git
+sudo cp ~/go/bin/gf /usr/bin/
+mkdir -p ~/.gf
+cd gf-patterns/
+cp *.json ~/.gf
+cd ..
